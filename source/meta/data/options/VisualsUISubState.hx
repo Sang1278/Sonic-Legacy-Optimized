51b30f9fdc8d7141009366ecc73c7969832dacbe
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-02-25 07:44:38 +0200

    update source/meta/data/options/VisualsUISubState.hx

--- source/meta/data/options/VisualsUISubState.hx
+++ source/meta/data/options/VisualsUISubState.hx
@@ -83,7 +83,6 @@ class VisualsUISubState extends BaseOptionsMenu
 		option.decimals = 1;
 		addOption(option);
 		
-		#if !mobile
 		var option:Option = new Option('FPS Counter',
 			'If unchecked, hides FPS Counter.',
 			'showFPS',
@@ -91,7 +90,6 @@ class VisualsUISubState extends BaseOptionsMenu
 			true);
 		addOption(option);
 		option.onChange = onChangeFPSCounter;
-		#end
 		
 		var option:Option = new Option('Pause Screen Song:',
 			"What song do you prefer for the Pause Screen?",
@@ -122,11 +120,9 @@ class VisualsUISubState extends BaseOptionsMenu
 		super.destroy();
 	}
 
-	#if !mobile
 	function onChangeFPSCounter()
 	{
 		if(Main.fpsVar != null)
 			Main.fpsVar.visible = ClientPrefs.data.showFPS;
 	}
-	#end
 }
