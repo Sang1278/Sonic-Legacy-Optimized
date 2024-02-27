193381eb7a2c632e8b8ba5a05a06e72e434d133e
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-02-27 15:15:25 +0200

    update source/meta/states/PlayState.hx

--- source/meta/states/PlayState.hx
+++ source/meta/states/PlayState.hx
@@ -1349,8 +1349,10 @@ class PlayState extends MusicBeatState
 			precacheList.set(Paths.formatToSongPath(ClientPrefs.data.pauseMusic), 'music');
 		}
 
+    #if desktop
 		// Updating Discord Rich Presence.
 		DiscordHandler.changePresence(detailsText, SONG.song.toLowerCase().replace('-', ' '), iconP2.getCharacter());
+     #end
 
 		if(!ClientPrefs.data.controllerMode)
 		{
@@ -1983,10 +1985,10 @@ class PlayState extends MusicBeatState
 		FlxTween.tween(timeBar, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
 		FlxTween.tween(timeTxt, {alpha: 1}, 0.5, {ease: FlxEase.circOut});
 
-
-
+    #if desktop
 		// Updating Discord Rich Presence (with Time Left)
 		DiscordHandler.changePresence(detailsText, SONG.song.toLowerCase().replace('-', ' '), iconP2.getCharacter(), true, songLength);
+      #end
 	
 		setOnScripts('songLength', songLength);
 		setOnScripts('barSongLength', barSongLength);
@@ -2625,7 +2627,7 @@ class PlayState extends MusicBeatState
 
 			PsychVideoSprite.globalResume();
 
-
+     #if desktop
 			if (startTimer != null && startTimer.finished)
 			{
 				DiscordHandler.changePresence(detailsText, SONG.song.toLowerCase().replace('-', ' '), iconP2.getCharacter(), true, songLength - Conductor.songPosition - ClientPrefs.data.noteOffset);
@@ -2634,13 +2636,14 @@ class PlayState extends MusicBeatState
 			{
 				DiscordHandler.changePresence(detailsText, SONG.song.toLowerCase().replace('-', ' '), iconP2.getCharacter());
 			}
-
+	  #end
 		}
 		callOnHScripts('onSubstateClose', []);
 
 		super.closeSubState();
 	}
 
+  #if desktop
 	override public function onFocus():Void
 	{
 
@@ -2671,6 +2674,7 @@ class PlayState extends MusicBeatState
 
 		super.onFocusLost();
 	}
+  #end
 
 	function resyncVocals():Void
 	{
@@ -2776,9 +2780,9 @@ class PlayState extends MusicBeatState
 				openSubState(new PauseSubState(boyfriend.getScreenPosition().x, boyfriend.getScreenPosition().y));
 				//}
 
-
+        #if desktop
 				DiscordHandler.changePresence(detailsPausedText, SONG.song.toLowerCase().replace('-', ' '), iconP2.getCharacter());
-
+        #end
 			}
 		}
 
@@ -3244,9 +3248,10 @@ class PlayState extends MusicBeatState
 
 				// MusicBeatState.switchState(new GameOverState(boyfriend.getScreenPosition().x, boyfriend.getScreenPosition().y));
 
-
+        #if desktop
 				// Game Over doesn't get his own variable because it's only used here
 				DiscordHandler.changePresence("Game Over - " + detailsText, SONG.song.toLowerCase().replace('-', ' '), iconP2.getCharacter());
+          #end
 
 				isDead = true;
 				return true;
