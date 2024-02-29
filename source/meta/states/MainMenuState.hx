69af1bde7794ab29cae6bfed9cb25e8cd55d1fed
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-02-29 20:54:18 +0200

    update source/meta/states/MainMenuState.hx

--- source/meta/states/MainMenuState.hx
+++ source/meta/states/MainMenuState.hx
@@ -49,6 +49,9 @@ class MainMenuState extends MusicBeatState
 
 	override function create()
 	{
+		Paths.clearStoredMemory();
+		Paths.clearUnusedMemory();
+
 		if (!fromTitle)
 		FlxG.sound.playMusic(ProgressionHandler.getMenuMusic());
 		fromTitle = true;
