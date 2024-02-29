d3296bb2a34ad6ec9e92ab36af2b985c3142d304
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-02-29 21:11:20 +0200

    update source/meta/data/options/OptionsState.hx

--- source/meta/data/options/OptionsState.hx
+++ source/meta/data/options/OptionsState.hx
@@ -39,18 +39,39 @@ class OptionsState extends MusicBeatState
 	function openSelectedSubstate(label:String) {
 		switch(label) {
 			case 'Notes':
+		    #if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new meta.data.options.NoteSettingsSubState());
 			case 'Controls':
+		    #if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new meta.data.options.ControlsSubState());
 			case 'Graphics':
+		    #if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new meta.data.options.GraphicsSettingsSubState());
 			case 'Visuals and UI':
+		    #if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new meta.data.options.VisualsUISubState());
 			case 'Gameplay':
+		    #if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new meta.data.options.GameplaySettingsSubState());
 			case 'Loading':
+		    #if mobile
+        removeVirtualPad();
+        #end
 				openSubState(new meta.data.options.MiscSubState());
 			case 'Adjust Delay and Combo':
+		    #if mobile
+        removeVirtualPad();
+        #end
 				LoadingState.loadAndSwitchState(new meta.data.options.NoteOffsetState());
 		}
 	}
@@ -92,6 +113,24 @@ class OptionsState extends MusicBeatState
 		changeSelection();
 		ClientPrefs.saveSettings();
 
+   #if mobile
+   addVirtualPad(UP_DOWN, A_B_X_Y);
+   #end
+
+   #if mobile
+		var sus:FlxText = new FlxText(10, 14, 0, 'Press X to customize your android controls', 16);
+		sus.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		sus.borderSize = 2.4;
+		sus.scrollFactor.set();
+		add(sus); //sus??!?!
+		
+		var sussy:FlxText = new FlxText(10, 32, 0, 'Press Y to customize opacity for your android controls', 16);
+		sussy.setFormat(Paths.font('vcr.ttf'), 16, FlxColor.WHITE, LEFT, FlxTextBorderStyle.OUTLINE, FlxColor.BLACK);
+		sussy.borderSize = 2.4;
+		sussy.scrollFactor.set();
+		add(sussy); //even more sus!
+		#end
+
 		super.create();
 	}
 
@@ -102,6 +141,18 @@ class OptionsState extends MusicBeatState
 
 	override function update(elapsed:Float) {
 		super.update(elapsed);
+
+    #if mobile
+		if (virtualPad.buttonX.justPressed)
+		{
+			removeVirtualPad();
+			openSubState(new mobile.MobileControlsSubState());
+		}
+		if (virtualPad.buttonY.justPressed) {
+			removeVirtualPad();
+			openSubState(new mobile.AndroidSettingsSubState());
+		}
+		#end
 
 		if (controls.UI_UP_P) {
 			changeSelection(-1);
