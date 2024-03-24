f21659ef254ba21a704c1153c140d1a2a0c9d4f5
MaysLastPlay10 <109207233+MaysLastPlay10@users.noreply.github.com>
2024-03-24 20:30:47 +0200

    update source/meta/data/options/MiscSubState.hx

--- source/meta/data/options/MiscSubState.hx
+++ source/meta/data/options/MiscSubState.hx
@@ -36,6 +36,29 @@ class MiscSubState extends BaseOptionsMenu
 		title = 'Misc';
 		rpcTitle = 'Miscellaneous Menu'; //for Discord Rich Presence
 
+		var maxThreads:Int = Std.parseInt(Sys.getEnv("NUMBER_OF_PROCESSORS"));
+
+			var option:Option = new Option('Multi-thread Loading', //Name
+				'If checked, the mod can use multiple threads to speed up loading times on some songs.\nRecommended to leave on, unless it causes crashing', //Description
+				'multicoreLoading', //Save data variable name
+				'bool', //Variable type
+				false
+			); //Default value
+			addOption(option);
+
+			var option:Option = new Option('Loading Threads', //Name
+				'How many threads the game can use to load graphics when using Multi-thread Loading.\nThe maximum amount of threads depends on your processor', //Description
+				'loadingThreads', //Save data variable name
+				'int', //Variable type
+				Math.floor(maxThreads/2)
+			); //Default value
+
+			option.minValue = 1;
+			option.maxValue = Std.parseInt(Sys.getEnv("NUMBER_OF_PROCESSORS"));
+			option.displayFormat = '%v';
+
+			addOption(option);
+
 		var option:Option = new Option('GPU Caching',
 		'If enabled, allows assets to be cached directly to the GPU. helps Performance',
 		'cacheOnGPU',
